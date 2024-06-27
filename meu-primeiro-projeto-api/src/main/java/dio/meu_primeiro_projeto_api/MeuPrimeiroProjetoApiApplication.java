85e6367a86017b3d1da3c26f186d950fcf421d18
Charles Thomaz <138392103+CharlesThomaz@users.noreply.github.com>
2024-06-26 21:41:23 -0300

    primeiroCommit

---
+++ meu-primeiro-projeto-api/src/main/java/dio/meu_primeiro_projeto_api/MeuPrimeiroProjetoApiApplication.java
@@ -0,0 +1,13 @@
+package dio.meu_primeiro_projeto_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoApiApplication.class, args);
+	}
+
+}
---
+++ meu-primeiro-projeto-api/src/test/java/dio/meu_primeiro_projeto_api/MeuPrimeiroProjetoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.meu_primeiro_projeto_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjetoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
